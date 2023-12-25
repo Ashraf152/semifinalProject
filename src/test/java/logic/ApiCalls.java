4bc9376cf082af8f850552c0bb4b23fc5cb66d36
Ashraf152 <ashraf.egbaria@gmail.com>
2023-12-25 09:56:46 +0200

    Api Calls

--- src/test/java/logic/ApiCalls.java
+++ src/test/java/logic/ApiCalls.java
@@ -33,7 +33,7 @@ public class ApiCalls {
     }
 
     public static WrapApiResponse deleteaddress()throws IOException{
-        String url = "https://www-api.rami-levy.co.il/api/v2/site/clubs/addresses/663488";
+        String url = "https://www-api.rami-levy.co.il/api/v2/site/clubs/addresses/663540";
         HashMap<String,String> tokenMap = new HashMap<>();
         tokenMap.put("Ecomtoken","eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3d3dy1hcGkucmFtaS1sZXZ5LmNvLmlsIiwiYXVkIjoiaHR0cHM6Ly93d3ctYXBpLnJhbWktbGV2eS5jby5pbCIsImlhdCI6MTcwMzQ1MjA4MS41NjIwNjgsIm5iZiI6MTcwMzQ1MjE0MS41NjIwNjgsImV4cCI6MTcwODYzNjA4MS41NjIwNjgsImlkIjo5MjEwNzUsImVtYWlsIjoiYXNocmFmLmVnYmFyaWFAZ21haWwuY29tIiwiY2lkIjoiOTkwMDE1NzkzMDEifQ.DgY2uM2GlpfC2KrLSDHBF8WoW4lx9PKeKgU7mBV45oc");
         return HttpFacade.sendHttpRequest(url,HttpMethod.DELETE,null,tokenMap,null);
@@ -49,28 +49,27 @@ public class ApiCalls {
     public static void main(String[]args)throws IOException{
 
         //add new address
-//        ApiCalls apiCalls=new ApiCalls();
-//        WrapApiResponse<AdressApiResponse> result=null;
-//        AdressBodyRequest adressBodyRequest=new AdressBodyRequest(null,2779,"חיפה","12","12","12","12",null,"12");
-//        result=ApiCalls.addNewAdress(adressBodyRequest.toString());
-//        System.out.println(result);
-//        result.setData(getJsonData(result.getData()));
-//        System.out.println(result.getData().getData().getAllAddresses().keySet());
-//        Object[] arr = result.getData().getData().getAllAddresses().keySet().toArray();
-//        System.out.println(arr[arr.length-1]);
+        ApiCalls apiCalls=new ApiCalls();
+        WrapApiResponse<AdressApiResponse> result=null;
+        AdressBodyRequest adressBodyRequest=new AdressBodyRequest(null,2779,"חיפה","12","12","12","12",null,"12");
+        result=ApiCalls.addNewAdress(adressBodyRequest.toString());
+        System.out.println(result);
+        result.setData(getJsonData(result.getData()));
+        System.out.println(result.getData().getData().getAllAddresses().keySet());
+       Object[] arr = result.getData().getData().getAllAddresses().keySet().toArray();
+        System.out.println(arr[arr.length-1]);
 
         //delete address
-      //  deleteaddress();
+   //     deleteaddress();
 
-        //add item to cart
+       // add item to cart
 //        ApiCalls apiCalls=new ApiCalls();
 //        WrapApiResponse<ApiResponse>result=null;
 //        String store="331";
 //        int inClub=0;
 //        String supplyAt= DateTimeFormat.getCurrentDateTime();
 //        HashMap<String,String> items= new HashMap<>();
-//        items.put("164854","1.00");
-//        items.put("410017","3.00");
+//        items.put("357141","3.00");
 //        ItemBodyRequest jsonBody=new ItemBodyRequest(store,inClub,supplyAt,items,null);
 //        System.out.println(jsonBody);
 //        result=ApiCalls.addNewProduct(jsonBody.toString());
