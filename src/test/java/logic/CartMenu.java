47b5066effe3ea884a70266cf9707075b6a2dde5
waseem <waseem-abusobe@hotmail.com>
2023-12-25 17:19:08 +0200

    remove main method from CartMenu

--- src/test/java/logic/CartMenu.java
+++ src/test/java/logic/CartMenu.java
@@ -7,14 +7,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
-
 import static Utils.ApiResponseParser.getJsonData;
 import static logic.ApiCalls.emptyCart;
-
 public class CartMenu {
     private final  String CART_BLOCK = "ul[data-v-1980ce6d]";
     private WebDriver driver;
@@ -41,39 +38,5 @@ public class CartMenu {
         }
         return count;
     }
-
-    public static void main(String[] args) throws InterruptedException, IOException {
-//        ApiCalls apiCalls=new ApiCalls();
-//        WrapApiResponse<ApiResponse>result=null;
-//        String store="331";
-//        int inClub=0;
-//        String supplyAt= DateTimeFormat.getCurrentDateTime();
-//        HashMap<String,String> items= new HashMap<>();
-//        items.put("310480","1.00");
-//        items.put("310482","2.00");
-//        items.put("164854","3.00");
-//        ItemBodyRequest jsonBody=new ItemBodyRequest(store,inClub,supplyAt,items,null);
-//        System.out.println(jsonBody);
-//        result=ApiCalls.addNewProduct(jsonBody.toString());
-//        System.out.println(result);
-
-        System.setProperty("webdriver.chrome.driver", "/Users/waseemabusobe/Desktop/chromedriver");
-        // Create a new instance of the ChromeDriver
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        // Navigate to the page containing the HTML snippet
-        driver.get("https://www.rami-levy.co.il/he");
-        MainPage mainPage = new MainPage(driver);
-        mainPage.flowPersonalArea("ashraf.egbaria@gmail.com","Ashrafadel152");
-        CartMenu cartMenu = new CartMenu(driver);
-        System.out.println(cartMenu.countItems());
-
-//        ItemBodyRequest jsonbody=new ItemBodyRequest("331",0,DateTimeFormat.getCurrentDateTime(),new HashMap<String,String>(),null);
-//        emptyCart(jsonbody.toString());
-        //ashraf.egbaria@gmail.com
-        //Ashrafadel152
-    }
-
-
 }
 
