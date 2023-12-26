a981de9ca9e5dccab721204333ad7d28d2311e9a
Ashraf152 <ashraf.egbaria@gmail.com>
2023-12-26 10:16:12 +0200

    ApiCalls.java HttpFacade.java

---
+++ src/test/java/logic/Header.java
@@ -0,0 +1,38 @@
+package logic;
+
+import Infrastructure.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Header extends BasePage {
+    private final String USER_BUTTON = "login-user";
+    private final String ADRESS_MANAGMENT="//div[a[@id='dashboard-addresses']]";
+    private final String PROFILE_DROPDOWN="//div[@class='position-relative- focus-item']";
+    private WebElement profileDropDownButton;
+    private WebElement userButton;
+    private WebElement addressManagement;
+
+
+    public Header(WebDriver driver) {
+        super(driver);
+        this.userButton = driver.findElement(By.id(USER_BUTTON));
+    }
+    public void clickOnLoginButton(){
+        this.userButton.click();
+    }
+    public void clickOnProfileDropDown(){
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        this.profileDropDownButton = wait.until(ExpectedConditions
+                .elementToBeClickable(By.xpath(PROFILE_DROPDOWN)));
+        this.profileDropDownButton.click();
+
+    }
+    public void clickOnAddressManagement(){
+        this.addressManagement= driver.findElement(By.xpath(ADRESS_MANAGMENT));
+        this.addressManagement.click();
+    }
+}
---
+++ src/test/java/logic/Login.java
@@ -0,0 +1,41 @@
+package logic;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+public class Login {
+    private final String EMAILID = "email";
+    private final String PASSWORDID = "password";
+    private final String ENTER_BUTTON_CSSS_ELECTOR = "button[aria-label='כניסה']";
+    private WebDriver driver;
+    private WebElement email;
+    private WebElement password;
+    private WebElement enterButton;
+    private Header header;
+    public Login(WebDriver driver) {
+        this.driver = driver;
+        header=new Header(driver);
+    }
+
+    public void clickOnLoginButton(){
+        header.clickOnLoginButton();
+    }
+    public void fillEmail(String email){
+        this.email = driver.findElement(By.id(EMAILID));
+        this.email.sendKeys(email);
+    }
+    public void fillPassword(String password){
+        this.password = driver.findElement(By.id(PASSWORDID));
+        this.password.sendKeys(password);
+    }
+    public void clickEnterButton(){
+        this.enterButton = driver.findElement(By.cssSelector(ENTER_BUTTON_CSSS_ELECTOR));
+        this.enterButton.click();
+    }
+    public void fullLoginProccess()  {
+        this.clickOnLoginButton();
+        this.fillEmail("ashraf.egbaria@gmail.com");
+        this.fillPassword("Ashrafadel152");
+        this.clickEnterButton();
+
+    }
+}
